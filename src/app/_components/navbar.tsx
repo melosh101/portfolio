4d45821d8de996099a25039ac2fa56cdb12d633c
RTS <6647530@RTS-365.dk>
2024-08-06 13:49:45 +0200

    transfer old

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import pfpImage from "../../public/pfp.png"
+import { cn } from "~/lib/utils";
+import Link from "next/link";
+
+interface navBarProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{
+
+}
+
+
+const Navbar = React.forwardRef<HTMLElement, navBarProps>(({className}, ref) => {
+    return <nav className={cn("flex items-center h-20 bg-slate-900", className)} ref={ref}>
+    <Link href="/" className={"pr-5"}>
+        <img src="pfp.png" alt="pfp" className="size-20"/>
+    </Link>
+    <Link href="/about" className="text-white bg-[#192441] hover:bg-[#3F51B5] font-medium rounded-lg text-sm mx-5 px-8 py-2.5 mr-2">About</Link>
+    <Link href="/projects" className="text-white bg-[#192441] hover:bg-[#3F51B5] font-medium rounded-lg text-sm mx-5 px-8 py-2.5 mr-2">Projects</Link>
+</nav>
+})
+
+Navbar.displayName = "Navbar"
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,50 +2,52 @@ import Link from "next/link";
 
 import { LatestPost } from "~/app/_components/post";
 import { api, HydrateClient } from "~/trpc/server";
+import Navbar from "./_components/navbar";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
-  void api.post.getLatest.prefetch();
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
+      <main className="text-white bg-slate-800 min-h-screen">
+        <div id="navbar" className='col-span-2'>
+          <Navbar className='' />
+        </div>
+        <div className="grid grid-cols-[2.5fr_1fr] h-full relative dark:bg-background">
+          <div id="gridContentWrapper" className="items-center self-start justify-end h-full ml-[40%] flex-nowrap">
+            <li className='font-bold list-none dark:text-white text-8xl mt-14 font-snippet ml-[30%]'>Melosh</li>
+            <li className='dark:text-white text-2xl mt-2 font-bold font-scp list-none ml-[40%]'>&lt;Code is life /&gt;</li>
+            <div className='grid self-end font-medium font-scp justify-around w-full h-full grid-cols-2 grid-rows-[auto_auto_auto] gap-4 list-none pt-9'>
+              <div className="w-full h-full col-span-1 p-2 rounded-md dark:text-white size-12 bg-primary">
+                <li className='text-xl'>Want to see more?</li>
+                <li className='text-l'>
+                  including blog posts, self-hosting projects and much more
+                </li>
               </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+              <div className="w-full h-full col-span-1 p-2 rounded-md dark:text-white size-12 bg-primary">
+                <li className='text-xl'>Want to see more?</li>
+                <li className='text-l'>
+                  including blog posts, self-hosting projects and much more
+                </li>
               </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-          </div>
+              <div className="w-full h-full col-span-1 p-2 rounded-md dark:text-white size-12 bg-primary">
+                <li className='text-xl'>Want to see more?</li>
+                <li className='text-l'>
+                  including blog posts, self-hosting projects and much more
+                </li>
+              </div>
+              <div className="w-full h-full col-span-1 p-2 rounded-md dark:text-white size-12 bg-primary">
+                <li className='text-xl'>Want to see more?</li>
+                <li className='text-l'>
+                  including blog posts, self-hosting projects and much more
+                </li>
+              </div>
+              <Link className='relative self-start list-none' href={"https://github.com/melosh101"} target="_blank">Check out my github</Link>
 
-          <LatestPost />
+            </div>
+          </div>
+          <div className='w-full h-full col-span-1 '>
+            <img src="/pfp.png" alt="" className=" mt-20 ml-12 w-[80%] h-[80%]" />
+          </div>
         </div>
       </main>
     </HydrateClient>
