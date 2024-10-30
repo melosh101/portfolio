c732922dbb6d9751ccbbd538c7ee1598a887daaf
RTS <RTS@melosh.tech>
2024-10-30 15:05:40 +0100

    fix sitemap url being wrong

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function Sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "www.milasholsting.dk",
+            url: "https://www.milasholsting.dk",
             lastModified: new Date(),
             changeFrequency: "monthly",
             priority: 1
